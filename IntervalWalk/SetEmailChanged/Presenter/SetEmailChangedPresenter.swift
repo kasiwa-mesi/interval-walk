971145edc353c32c791011fe51cae5c7c9489cde
kasiwa-mesi <italynataly@outlook.jp>
2023-02-22 14:26:22 +0900

    :lipstick: メールアドレス変更画面のUIを実装

---
+++ IntervalWalk/SetEmailChanged/Presenter/SetEmailChangedPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  SetEmailChangedPresenter.swift
+//  IntervalWalk
+//
+//  Created by kasiwa on 2023/02/22.
+//
+
+import Foundation
---
+++ IntervalWalk/SetEmailChanged/ViewController/SetEmailChangedViewController.swift
@@ -0,0 +1,11 @@
+//
+//  SetEmailChangedViewController.swift
+//  IntervalWalk
+//
+//  Created by kasiwa on 2023/02/22.
+//
+
+import UIKit
+
+final class SetEmailChangedViewController: UIViewController {
+}
