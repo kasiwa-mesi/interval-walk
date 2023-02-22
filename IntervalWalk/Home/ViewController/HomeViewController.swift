f7b2c4f722da6008f4e5dca7b2e5e1080f99359a
kasiwa-mesi <italynataly@outlook.jp>
2023-02-22 19:06:02 +0900

    :sparkles: Home画面のUI, 機能を実装

--- IntervalWalk/Home/ViewController/HomeViewController.swift
+++ IntervalWalk/Home/ViewController/HomeViewController.swift
@@ -8,8 +8,19 @@
 import UIKit
 
 final class HomeViewController: UIViewController {
+    private var signOutButtonItem: UIBarButtonItem!
+    
+    private var presenter: HomePresenterInput!
+    func inject(presenter: HomePresenterInput) {
+      self.presenter = presenter
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        signOutButtonItem = UIBarButtonItem(barButtonSystemItem: .reply, target: self, action: #selector(tapSignOutButton))
+        
+        self.navigationItem.leftBarButtonItem = signOutButtonItem
     }
     
     static func makeFromStoryboard() -> HomeViewController {
@@ -17,5 +28,20 @@ final class HomeViewController: UIViewController {
             fatalError()
         }
         return vc
+    }
+}
+
+@objc private extension HomeViewController {
+    func tapSignOutButton() {
+        // presenterを利用してログアウト機能を実装する
+        presenter.logOut()
+    }
+}
+
+extension HomeViewController: HomePresenterOutput {
+    func showErrorAlert(code: String, message: String) {
+        let gotItAction = UIAlertAction(title: String.ok, style: .default)
+        let errorTitle = String.errorTitle + code
+        self.showAlert(title: errorTitle, message: message, actions: [gotItAction])
     }
 }
