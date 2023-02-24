b69ab8811dcf85e6049b799c5ecdb16c6db6955b
kasiwa-mesi <italynataly@outlook.jp>
2023-02-25 00:13:01 +0900

    :sparkles: Trial画面を実装

---
+++ IntervalWalk/Trial/Presenter/TrialPresenter.swift
@@ -0,0 +1,226 @@
+//
+//  TrialPresenter.swift
+//  IntervalWalk
+//
+//  Created by kasiwa on 2023/02/24.
+//
+
+import Foundation
+import AVFoundation
+
+protocol TrialPresenterInput {
+    var state: State { get }
+    var timerLabel: String { get }
+    var player: AVAudioPlayer? { get }
+    var hour: Int { get }
+    var minute: Int { get }
+    var second: Int { get }
+    var userId: String { get }
+    func showErrorAlert(error: NSError?)
+    func getStartButtonTitle() -> String
+    func setTimerLabelUpdated()
+    func setStateChanged()
+    func reset()
+    func playStartSound()
+}
+
+protocol TrialPresenterOutput {
+    func showErrorAlert(code: String, message: String)
+    func updateTimerLabel()
+    func updateWalkingImage(name: String)
+    func setSpeedLabelSpeedy()
+    func setSpeedLabelSlow()
+}
+
+
+final class TrialPresenter {
+    private var _state: State
+    var state: State {
+        get {
+            return _state
+        }
+    }
+    
+    private var _timerLabel: String
+    var timerLabel: String {
+        get {
+            return _timerLabel
+        }
+    }
+    
+    private var _player: AVAudioPlayer?
+    var player: AVAudioPlayer? {
+        get {
+            return _player
+        }
+    }
+    
+    private var _hour: Int
+    var hour: Int {
+        get {
+            return _hour
+        }
+    }
+    
+    private var _minute: Int
+    var minute: Int {
+        get {
+            return _minute
+        }
+    }
+    
+    private var _second: Int
+    var second: Int {
+        get {
+            return _second
+        }
+    }
+    
+    private var _userId: String
+    var userId: String {
+        get {
+            return _userId
+        }
+    }
+    
+    private var output: TrialPresenterOutput!
+    init(output: TrialPresenterOutput) {
+        self._state = .idle
+        self._timerLabel = "00:00:00"
+        self._hour = 0
+        self._minute = 0
+        self._second = 0
+        
+        let userId = AuthService.shared.getCurrentUserId()
+        self._userId = userId ?? ""
+        
+        self.output = output
+    }
+    
+    private func scheduleTimer() -> Timer {
+        Timer.scheduledTimer(
+            withTimeInterval: 1.0,
+            repeats: true,
+            block: { [weak self] _ in
+                print("block")
+                self?.output.updateTimerLabel()
+            }
+        )
+    }
+    
+    private func playSound(name: String) {
+        guard let soundURL = Bundle.main.url(forResource: name, withExtension: "mp3") else {
+            print("音声読み込めない")
+            return
+        }
+        do {
+            _player = try AVAudioPlayer(contentsOf: soundURL)
+            _player?.play()
+        } catch let error as NSError {
+            output.showErrorAlert(code: String(error.code), message: error.localizedDescription)
+            print("error")
+        }
+    }
+}
+
+extension TrialPresenter: TrialPresenterInput {
+    func showErrorAlert(error: NSError?) {
+        if let error {
+            output.showErrorAlert(code: String(error.code), message: error.localizedDescription)
+            return
+        }
+    }
+    
+    func getStartButtonTitle() -> String {
+        switch state {
+        case .idle, .pause:
+            return "開始"
+        case .running:
+            return "停止"
+        }
+    }
+    
+    func setTimerLabelUpdated() {
+        let elapsedTime = state.elapsedTime(now: Date())
+        let fixedElapsedTime = floor(Double(elapsedTime))
+        
+        _hour = Int(elapsedTime) / 3600
+        _minute = Int(elapsedTime) / 60
+        _second = Int(elapsedTime) % 60
+        
+        print(fixedElapsedTime)
+        print(hour, minute, second)
+        
+        if (minute + 1) % 6 == 0 && second == 54 {
+            print("ゆっくりに切り替える")
+            playSound(name: "slow")
+        } else if (minute + 1) % 3 == 0 && second == 54 {
+            print("早歩きに切り替える")
+            playSound(name: "speedy")
+        }
+        
+        if minute != 0 && minute % 6 == 0 && second == 0 {
+            output.setSpeedLabelSlow()
+            output.updateWalkingImage(name: "slow")
+            print("ゆっくりに切り替える")
+        } else if minute != 0 && minute % 3 == 0 && second == 0 {
+            output.setSpeedLabelSpeedy()
+            output.updateWalkingImage(name: "speedy")
+            print("早歩きに切り替える")
+        }
+        print(elapsedTime)
+        
+        _timerLabel = String(format: "%02d:%02d:%02d", hour, minute, second)
+    }
+    
+    func setStateChanged() {
+        switch state {
+        case .idle:
+            let runningState = RunningStateModel(
+                elapsedTime: 0,
+                timer: scheduleTimer()
+            )
+            _state = .running(runningState)
+            
+        case let .running(runningState):
+            print("Timerを止める")
+            runningState.timer.invalidate()
+            _state = .pause(
+                PauseStateModel(
+                    elapsedTime: runningState.elapsedTime + Date().timeIntervalSince(runningState.startDate)
+                )
+            )
+            
+        case let .pause(pauseState):
+            _state = .running(
+                RunningStateModel(
+                    elapsedTime: pauseState.elapsedTime,
+                    timer: scheduleTimer()
+                )
+            )
+        }
+    }
+    
+    func reset() {
+        switch state {
+        case .idle:
+            Router.shared.showReStart()
+        case let .running(runningState):
+            runningState.timer.invalidate()
+            _state = .idle
+            Router.shared.showReStart()
+        case .pause:
+            _state = .idle
+            Router.shared.showReStart()
+        }
+    }
+    
+    func playStartSound() {
+        switch state {
+        case .idle, .pause:
+            playSound(name: "start")
+        case .running:
+            break
+        }
+    }
+}
---
+++ IntervalWalk/Trial/ViewController/TrialViewController.swift
@@ -0,0 +1,98 @@
+//
+//  TrialViewController.swift
+//  IntervalWalk
+//
+//  Created by kasiwa on 2023/02/24.
+//
+
+import UIKit
+import AVFoundation
+
+final class TrialViewController: UIViewController {
+    @IBOutlet private weak var speedLabel: UILabel!
+    @IBOutlet private weak var timerLabel: UILabel!
+    @IBOutlet private weak var walkingImage: UIImageView!
+    @IBOutlet private weak var startButton: UIButton! {
+        didSet {
+            startButton.addTarget(self, action: #selector(tapStartButton), for: .touchUpInside)
+        }
+    }
+    @IBOutlet private weak var finishButton: UIButton! {
+        didSet {
+            finishButton.addTarget(self, action: #selector(tapFinishButton), for: .touchUpInside)
+        }
+    }
+    
+    private var presenter: TrialPresenterInput!
+    func inject(presenter: TrialPresenterInput) {
+      self.presenter = presenter
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        timerLabel.font = UIFont.monospacedDigitSystemFont(ofSize: 50, weight: .bold)
+
+        updateStartButton()
+    }
+    
+    static func makeFromStoryboard() -> TrialViewController {
+        guard let vc = UIStoryboard.init(name: "Trial", bundle: nil).instantiateInitialViewController() as? TrialViewController else {
+            fatalError()
+        }
+        return vc
+    }
+}
+
+private extension TrialViewController {
+    func updateStartButton() {
+        let title = presenter.getStartButtonTitle()
+        startButton.setTitle(title, for: .normal)
+        
+        updateTimerLabel()
+    }
+}
+
+@objc private extension TrialViewController {
+    func tapStartButton() {
+        presenter.playStartSound()
+        presenter.setStateChanged()
+        updateStartButton()
+    }
+    
+    func tapFinishButton() {
+        presenter.reset()
+        updateStartButton()
+    }
+}
+
+extension TrialViewController: TrialPresenterOutput {
+    func showErrorAlert(code: String, message: String) {
+        let gotItAction = UIAlertAction(title: String.ok, style: .default) { _ in
+            Router.shared.showReStart()
+        }
+        let errorTitle = String.errorTitle + code
+        self.showAlert(title: errorTitle, message: message, actions: [gotItAction])
+    }
+    
+    func updateTimerLabel() {
+        presenter.setTimerLabelUpdated()
+        timerLabel.text = presenter.timerLabel
+    }
+    
+    func setSpeedLabelSpeedy() {
+        speedLabel.text = "早歩き"
+        speedLabel.textColor = UIColor(red: 0.92, green: 0.52, blue: 0.52, alpha: 1.0)
+        timerLabel.textColor = UIColor(red: 0.92, green: 0.52, blue: 0.52, alpha: 1.0)
+    }
+    
+    func setSpeedLabelSlow() {
+        speedLabel.text = "ゆっくり"
+        speedLabel.textColor = UIColor(red: 0.37, green: 0.53, blue: 0.77, alpha: 1.0)
+        timerLabel.textColor = UIColor(red: 0.37, green: 0.53, blue: 0.77, alpha: 1.0)
+    }
+    
+    func updateWalkingImage(name: String) {
+        walkingImage.image = UIImage(named: name)
+    }
+}
